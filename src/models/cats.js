24c3ee78ceac44ad8765c61a6a9f75d647cecf08
Siddharth Chugh <63421456+Syd25@users.noreply.github.com>
2020-08-31 01:48:42 +0530

    Add files via upload

---
+++ src/models/cats.js
@@ -0,0 +1,38 @@
+const cats = [
+    {
+        id: 'cat1',
+        name: 'Tony',
+        src: `${window.location.origin}/images/cat1.jpg`,
+        age:0,
+        clicks: 0
+    },
+    {
+        id: 'cat2',
+        name: 'Steve',
+        src: `${window.location.origin}/images/cat2.jpg`,
+        age:0,
+        clicks: 0
+    },
+    {
+        id: 'cat3',
+        name: 'Bruce',
+        src: `${window.location.origin}/images/cat3.jpg`,
+        age:0,
+        clicks: 0
+    },
+    {
+        id: 'cat4',
+        name: 'Natasha',
+        src: `${window.location.origin}/images/cat4.jpg`,
+        age:0,
+        clicks: 0
+    },
+    {
+        id: 'cat5',
+        name: 'Clint',
+        src: `${window.location.origin}/images/cat5.jpg`,
+        clicks: 0
+    }
+];
+
+export default cats;
